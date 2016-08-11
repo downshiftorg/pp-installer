03b5fd6e4074144f0695bda57b47e40776724583
Jared Henderson <jared@netrivet.com>
2016-08-10 21:55:17 -0400

    fix display of non-test-drive theme name everywhere

--- views/notice-test-driving.php
+++ views/notice-test-driving.php
@@ -2,7 +2,7 @@
     <p style="margin: 0.75em">
         You are currently <b style="text-decoration: underline;">test-driving ProPhoto 6</b>.
         Logged-in users can see and customize P6, but all normal site visitors will see
-        your site with the <b><?php echo ppi_get_theme_name(); ?> theme</b> active.
+        your site with the <b><?php echo ppi_get_non_test_drive_theme_name(); ?> theme</b> active.
         <a href="<?php echo ppi_get_admin_page_url(); ?>">Click here</a> for more options.
     </p>
 </div>
