8d44fbb26787b78ff4835d02237905fe717bf5ba
Josh Arens <arens.joshua@sbcglobal.net>
2018-05-18 17:02:07 -0400

    bump version

--- pp-installer.php
+++ pp-installer.php
@@ -4,7 +4,7 @@ Plugin Name: ProPhoto 7 Installer
 Plugin URI: https://github.com/downshiftorg/pp-installer
 Description: Theme installer plugin for ProPhoto version 7. Checks server compatibility, auto-registers, and allows test-driving Prophoto while safely keeping another theme active.
 Author: ProPhoto
-Version: 7.0.1
+Version: 7.0.2
 Author URI: https://pro.photo
 License: MIT
  */
